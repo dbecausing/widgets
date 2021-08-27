5c0566e534bdc7ea203ba771d7c45c5e48d0e3bf
Damian Wantzing <damianwantzing@gmail.com>
2021-08-27 21:25:42 +0200

    Added Search component

---
+++ src/components/Search.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+	const [term, setTerm] = useState("");
+
+	useEffect(() => {
+		const search = async () => {
+			await axios.get("https://en.wikipedia.org/w/api.php", {
+				params: {
+					action: "query",
+					list: "search",
+					origin: "*",
+					format: "json",
+					srsearch: term,
+				},
+			});
+		};
+
+		search();
+	}, [term]);
+
+	return (
+		<div>
+			<div className="ui form">
+				<div className="field">
+					<form onSubmit={(e) => e.preventDefault()}>
+						<label>Enter search term</label>
+						<input
+							value={term}
+							className="input"
+							type="text"
+							onChange={(e) => setTerm(e.target.value)}
+						/>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Search;
