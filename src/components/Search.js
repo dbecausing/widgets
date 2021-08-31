69347c0557c4647b48ba66c377a604f5bc314e7b
db <damianwantzing@gmail.com>
2021-08-31 17:58:22 +0200

    finished app w/ navigation

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,22 +3,61 @@ import axios from "axios";
 
 const Search = () => {
 	const [term, setTerm] = useState("");
+	const [debouncedTerm, setDebouncedTerm] = useState(term);
+	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const timerId = setTimeout(() => {
+			setDebouncedTerm(term);
+		}, 500);
+
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [term]);
 
 	useEffect(() => {
 		const search = async () => {
-			await axios.get("https://en.wikipedia.org/w/api.php", {
-				params: {
-					action: "query",
-					list: "search",
-					origin: "*",
-					format: "json",
-					srsearch: term,
-				},
-			});
-		};
+			const { data } = await axios.get(
+				"https://en.wikipedia.org/w/api.php",
+				{
+					params: {
+						action: "query",
+						list: "search",
+						origin: "*",
+						format: "json",
+						srsearch: debouncedTerm,
+					},
+				}
+			);
 
-		search();
-	}, [term]);
+			setResults(data.query.search);
+		};
+		if (debouncedTerm) {
+			search();
+		}
+	}, [debouncedTerm]);
+
+	const renderedResults = results.map((result) => {
+		return (
+			<div key={result.pageid} className="item">
+				<div className="right floated content">
+					<a
+						className="ui button"
+						href={`https://en.wikipedia.org?curid=${result.pageid}`}
+					>
+						Go
+					</a>
+				</div>
+				<div className="content">
+					<div className="header">{result.title}</div>
+					<span
+						dangerouslySetInnerHTML={{ __html: result.snippet }}
+					></span>
+				</div>
+			</div>
+		);
+	});
 
 	return (
 		<div>
@@ -35,6 +74,7 @@ const Search = () => {
 					</form>
 				</div>
 			</div>
+			<div className="ui celled list">{renderedResults}</div>
 		</div>
 	);
 };
